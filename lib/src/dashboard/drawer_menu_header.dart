75e494c68a79ebb719a1327315a591edfb02de9c
alann-maulana <alann_it06@yahoo.co.id>
2021-09-29 14:09:41 +0700

    add logo to title

--- lib/src/dashboard/drawer_menu_header.dart
+++ lib/src/dashboard/drawer_menu_header.dart
@@ -7,17 +7,27 @@ class DrawerMenuHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final title = Text.rich(TextSpan(
+      children: [
+        const WidgetSpan(child: FlutterLogo()),
+        const TextSpan(text: '  '),
+        TextSpan(text: AppLocalizations.of(context)!.appTitle),
+      ],
+    ));
+
     return ResponsiveView(
       builder: (BuildContext context) {
         return UserAccountsDrawerHeader(
-          accountName: Text(AppLocalizations.of(context)!.appTitle),
+          accountName: title,
           accountEmail: const Text('kangmas.alan@gmail.com'),
         );
       },
       tablet: (BuildContext context) {
         return AppBar(
           automaticallyImplyLeading: false,
-          title: Text(AppLocalizations.of(context)!.appTitle),
+          title: title,
+          centerTitle: true,
+          elevation: 0,
         );
       },
     );
