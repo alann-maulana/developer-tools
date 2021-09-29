cf9359f8a72a642537b1db614a4d0c9cf9e26603
alann-maulana <alann_it06@yahoo.co.id>
2021-09-29 13:22:29 +0700

    enable options on json to dart, split ui on phone or desktop

--- lib/src/json/jsontodart_controller.dart
+++ lib/src/json/jsontodart_controller.dart
@@ -5,15 +5,38 @@ import 'package:flutter/services.dart';
 import 'package:json_to_dart/json_to_dart.dart';
 
 class JsonToDartController extends ChangeNotifier {
+  final TextEditingController classController = TextEditingController();
   final TextEditingController inputController = TextEditingController();
   final TextEditingController outputController = TextEditingController();
 
   String? inputError;
 
+  bool _privateField = false;
+  bool _finalField = true;
+
+  bool get privateField => _privateField;
+  set privateField(bool value) {
+    _privateField = value;
+    onInputChanged(inputController.text);
+  }
+
+  bool get finalField => _finalField;
+  set finalField(bool value) {
+    _finalField = value;
+    onInputChanged(inputController.text);
+  }
+
   clear() {
+    classController.text = '';
     inputController.text = '';
     outputController.text = '';
     notifyListeners();
+  }
+
+  onClassChanged(String input) {
+    if (inputController.text.isNotEmpty) {
+      onInputChanged(inputController.text);
+    }
   }
 
   onInputChanged(String input) {
@@ -31,7 +54,13 @@ class JsonToDartController extends ChangeNotifier {
       return;
     }
 
-    final classGenerator = ModelGenerator('Sample');
+    final className =
+        classController.text.isEmpty ? 'UnknownClassName' : classController.text;
+    final classGenerator = ModelGenerator(
+      className,
+      privateFields: privateField,
+      finalFields: finalField,
+    );
     final dartCode = classGenerator.generateDartClasses(input);
     outputController.text = dartCode.code;
     notifyListeners();
--- lib/src/json/jsontodart_view.dart
+++ lib/src/json/jsontodart_view.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:developer_tools/src/dashboard/drawer_menu.dart';
+import 'package:developer_tools/src/views/responsive_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_native/flutter_native.dart';
@@ -36,67 +39,141 @@ class JsonToDartBody extends StatelessWidget {
       child: AnimatedBuilder(
         animation: controller,
         builder: (context, child) {
-          return Column(
-            children: [
-              Row(
-                children: [
-                  const Text('Input :'),
-                  const SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: controller.onPaste,
-                    child: const Text('Clipboard'),
+          final childrenInput = [
+            Row(
+              children: [
+                const Text('Input :'),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: controller.onPaste,
+                  child: const Text('Clipboard'),
+                ),
+                const SizedBox(width: 8),
+                OutlinedButton(
+                  onPressed: controller.clear,
+                  child: const Text('Clear'),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: TextField(
+                    controller: controller.classController,
+                    maxLines: 1,
+                    decoration: const InputDecoration(
+                      hintText: 'Dart class name',
+                      isDense: true,
+                    ),
+                    onChanged: controller.onClassChanged,
+                    style: const TextStyle(
+                      fontFeatures: [FontFeature.tabularFigures()],
+                    ),
                   ),
-                  const SizedBox(width: 8),
-                  OutlinedButton(
-                    onPressed: controller.clear,
-                    child: const Text('Clear'),
-                  ),
-                  const Spacer(),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Expanded(
-                child: TextField(
-                  controller: controller.inputController,
-                  maxLines: null,
-                  expands: true,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    errorText: controller.inputError,
-                  ),
-                  onChanged: controller.onInputChanged,
-                  textAlignVertical: TextAlignVertical.top,
+                ),
+                PopupMenuButton<String>(
+                  tooltip: 'Options',
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (BuildContext context) {
+                    return <PopupMenuEntry<String>>[
+                      CheckedPopupMenuItem(
+                        value: 'final',
+                        checked: controller.finalField,
+                        child: const Text('Final Field'),
+                      ),
+                      CheckedPopupMenuItem(
+                        value: 'private',
+                        checked: controller.privateField,
+                        child: const Text('Private Field'),
+                      ),
+                      const PopupMenuDivider(),
+                      const CheckedPopupMenuItem(
+                        value: 'private',
+                        checked: true,
+                        child: Text('Null Safety'),
+                        enabled: false,
+                      ),
+                    ];
+                  },
+                  onSelected: (value) {
+                    if (value == 'final') {
+                      controller.finalField = !controller.finalField;
+                    } else if (value == 'private') {
+                      controller.privateField = !controller.privateField;
+                    }
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: TextField(
+                controller: controller.inputController,
+                maxLines: null,
+                expands: true,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  errorText: controller.inputError,
+                ),
+                onChanged: controller.onInputChanged,
+                textAlignVertical: TextAlignVertical.top,
+                style: const TextStyle(
+                  fontFeatures: [FontFeature.tabularFigures()],
                 ),
               ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  const Text('Output :'),
-                  const Spacer(),
-                  ElevatedButton.icon(
-                    onPressed: () async {
-                      await controller.onCopy();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Output copied')));
-                    },
-                    label: const Text('Copy'),
-                    icon: const Icon(Icons.copy),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Expanded(
-                child: TextField(
-                  controller: controller.outputController,
-                  maxLines: null,
-                  expands: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  textAlignVertical: TextAlignVertical.top,
+            ),
+          ];
+
+          final childrenOutput = [
+            Row(
+              children: [
+                const Text('Output :'),
+                const Spacer(),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    await controller.onCopy();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Output copied')));
+                  },
+                  label: const Text('Copy'),
+                  icon: const Icon(Icons.copy),
                 ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: TextField(
+                controller: controller.outputController,
+                maxLines: null,
+                expands: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                textAlignVertical: TextAlignVertical.top,
+                style: const TextStyle(
+                  fontFeatures: [FontFeature.tabularFigures()],
+                ),
+                readOnly: true,
               ),
-            ],
+            ),
+          ];
+
+          return ResponsiveView(
+            builder: (context) {
+              return Column(
+                children: [
+                  ...childrenInput,
+                  const SizedBox(height: 16),
+                  ...childrenOutput,
+                ],
+              );
+            },
+            tablet: (context) {
+              return Row(
+                children: [
+                  Expanded(child: Column(children: childrenInput)),
+                  const VerticalDivider(width: 32),
+                  Expanded(child: Column(children: childrenOutput)),
+                ],
+              );
+            },
           );
         },
       ),
